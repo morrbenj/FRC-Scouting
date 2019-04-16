8b6b35c15c84dd711f95ed73b65ed92b3d59db27
morrbenj <morrbenj19@gmail.com>
2019-04-16 11:16:42 -0400

    Initial commit

---
+++ Data/API_Score.py
@@ -0,0 +1,50 @@
+class Score:
+    def __init__(self, data):
+        self.alliance = self.data.get('alliance')
+        self.preMatchLevelRobot1 = self.data.get('preMatchLevelRobot1')
+        self.habLineRobot1 = self.data.get('habLineRobot1')
+        self.endgameRobot1 = self.data.get('endgameRobot1')
+
+        self.preMatchLevelRobot2 = self.data.get('preMatchLevelRobot2')
+        self.habLineRobot2 = self.data.get('habLineRobot2')
+        self.endgameRobot2 = self.data.get('endgameRobot2')
+
+        self.preMatchLevelRobot3 = self.data.get('preMatchLevelRobot3')
+        self.habLineRobot3 = self.data.get('habLineRobot3')
+        self.endgameRobot3 = self.data.get('endgameRobot3')
+
+        self.topLeftRocketNear = self.data.get('topLeftRocketNear')
+        self.topRightRocketNear = self.data.get('topRightRocketNear')
+        self.midLeftRocketNear = self.data.get('midLeftRocketNear')
+        self.midRightRocketNear = self.data.get('midRightRocketNear')
+        self.lowLeftRocketNear = self.data.get('lowLeftRocketNear')
+        self.lowRightRocketNear = self.data.get('lowRightRocketNear')
+        self.completedRocketNear = self.data.get('completedRocketNear')
+        
+        self.topLeftRocketFar = self.data.get('topLeftRocketFar')
+        self.topRightRocketFar = self.data.get('topRightRocketFar')
+        self.midLeftRocketFar = self.data.get('midLeftRocketFar')
+        self.midRightRocketFar = self.data.get('midRightRocketFar')
+        self.lowLeftRocketFar = self.data.get('lowLeftRocketFar')
+        self.lowRightRocketFar = self.data.get('lowRightRocketFar')
+        self.completedRocketFar = self.data.get('completedRocketFar')
+
+        self.preMatchBay1 = self.data.get('preMatchBay1')
+        self.preMatchBay2 = self.data.get('preMatchBay2')
+        self.preMatchBay3 = self.data.get('preMatchBay3')
+        self.preMatchBay4 = self.data.get('preMatchBay4')
+        self.preMatchBay5 = self.data.get('preMatchBay5')
+        self.preMatchBay6 = self.data.get('preMatchBay6')
+        self.preMatchBay7 = self.data.get('preMatchBay7')
+        self.preMatchBay8 = self.data.get('preMatchBay8')
+
+        self.bay1 = self.data.get('bay1')
+        self.bay2 = self.data.get('bay2')
+        self.bay3 = self.data.get('bay3')
+        self.bay4 = self.data.get('bay4')
+        self.bay5 = self.data.get('bay5')
+        self.bay6 = self.data.get('bay6')
+        self.bay7 = self.data.get('bay7')
+        self.bay8 = self.data.get('bay8')
+
+        
